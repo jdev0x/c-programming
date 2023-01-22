b1a469fc4f8fa2ee7a32203f0085b696a88be2eb
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-01-22 14:56:42 +0000

    Update 001-word-and-word-size.c

--- 01-introduction-to-data/001-word-and-word-size.c
+++ 01-introduction-to-data/001-word-and-word-size.c
@@ -1,18 +1,17 @@
-/* Word (block) */
-/* Word Size */
+/* word (block) */
+/* word size */
 
 
 int main(){
     
     unsigned char v1  = 0x00;
     unsigned char v2  = 0xFF;
-
     unsigned short v3 = 0x0000;
-
     unsigned int v4 = 0xFF2C37AE;
-    // v1 = 10;
+    
     unsigned char bitmask = 0b00001010;
     unsigned char bitmask_2 = 0b11001111;
+    
     v1 |= bitmask;
     v2 &= bitmask_2; 
     v3 |= 0x0300;
@@ -21,6 +20,5 @@ int main(){
     v4 |= 0x00000800;
     v4 &= 0xAFFFFFFF;
     
-
     return 0;
 }
