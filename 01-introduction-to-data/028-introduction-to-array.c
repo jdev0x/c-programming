a237067ee04c1eb8b3e4369f3118c6ed5fd08968
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-02-06 17:11:09 +0300

    Create 028-introduction-to-array.c

---
+++ 01-introduction-to-data/028-introduction-to-array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    signed char values[4];
+    values[0]= 10;
+    values[1]= 20;
+    values[2]= 30;
+    values[3]= 40;
+    
+    printf("%d %d %d %d\n", values[0], values[1], values[2],values[3]);
+
+
+    return 0;
+}
