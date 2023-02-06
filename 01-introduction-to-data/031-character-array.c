91f3b704cbe2d60909ec9ad738ee82644dd9c36c
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-02-06 17:38:47 +0300

    Create 031-character-array.c

---
+++ 01-introduction-to-data/031-character-array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char name[4] = {'A', 66, 'C', 'D'};
+    char message [] = "Welcome to C";
+    printf("%c\n", name[1]);
+    printf("%s\n", message);
+    
+    return 0;
+}
