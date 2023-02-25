b331544658b89fa9e4f3d14432c49ab4153f2821
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-02-25 15:45:47 +0300

    Update 061-jascii-project-part-01.c

--- 01-introduction-to-data/061-jascii-project-part-01.c
+++ 01-introduction-to-data/061-jascii-project-part-01.c
@@ -2,22 +2,22 @@
 
  int main(int argc, char *argv[]){
     if(argc != 3){
-        printf("ERROR: usage <%s d|h value>\n", argv[0]);
+        printf("ERROR: usage <%s b|o|d|h value>\n", argv[0]);
         return 0;
     }
-    if(argv[1][0] == 'd' && argv[1][0] == 'D'){
+    if(argv[1][0] == 'd' || argv[1][0] == 'D'){
         for(char * cptr = argv[2]; *cptr != '\0'; cptr++){
             printf("%d ", *cptr);
         }
-    }else if(argv[1][0] == 'h' && argv[1][0] == 'H'){
+    }else if(argv[1][0] == 'h' || argv[1][0] == 'H'){
         for(char * cptr = argv[2]; *cptr != '\0'; cptr++){
             printf("0x%x ", *cptr);
         }
-    }else if(argv[1][0] == 'o' && argv[1][0] == 'O'){
+    }else if(argv[1][0] == 'o' || argv[1][0] == 'O'){
         for(char * cptr = argv[2]; *cptr != '\0'; cptr++){
             printf("%o ", *cptr);
         }
-    }else if(argv[1][0] == 'b' && argv[1][0] == 'B'){
+    }else if(argv[1][0] == 'b' || argv[1][0] == 'B'){
         for(char * cptr = argv[2]; *cptr != '\0'; cptr++){
             char ch = *cptr;
             unsigned char bitmask = 0x80;
