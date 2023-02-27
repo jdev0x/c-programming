b4bfb77ca7843eb028db9dcda4f56993dcc78a92
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-02-27 17:58:14 +0300

    Update 001-if-statement.c

--- 02-statements/001-if-statement.c
+++ 02-statements/001-if-statement.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(){
+    int x = 5;
+    if(x>3){
+        printf("its greater\n");
+    }
+    x = 123;
+    if(x < 150){
+        printf("The x is less than 150.\n");
+    }
+    if(x<199){
+        printf("it");
+        printf("is");
+        printf("less");
+    }
+    if(x<150){
+        printf("The x is less than 150.\n");
+        printf("This is a second statement\n");
+    }
+    return 0;
+}
