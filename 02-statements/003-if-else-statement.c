f5a345c38641f901f6e3cb29ac7d91ce4cb7b0c5
jdev0x <119949928+jdev0x@users.noreply.github.com>
2023-02-28 16:58:35 +0300

    Create 003-if-else-statement.c

---
+++ 02-statements/003-if-else-statement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int value = 10;
+    // value -> 10(A) else (B)
+    if(value == 93){
+        printf("A\n");
+    }else{
+        printf("B\n");
+    }
+    return 0;
+}
